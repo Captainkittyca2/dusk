526b24e8c8052a88473ed03a504322620fa32948
Captain Kitty Cat <ikelink2015@outlook.com>
2026-05-14 21:01:21 +0300

    Merge remote branch 'origin/main'

--- src/dusk/imgui/ImGuiConsole.hpp
+++ src/dusk/imgui/ImGuiConsole.hpp
@@ -7,7 +7,6 @@
 
 #include <aurora/aurora.h>
 
-#include "ImGuiMenuGame.hpp"
 #include "ImGuiMenuTools.hpp"
 #include "ImGuiCatDeluxe.hpp"
 #include "dusk/main.h"
@@ -44,8 +43,6 @@ private:
     ImGuiWindow* m_dragScrollWindow = nullptr;
     ImVec2 m_dragScrollLastMousePos = {};
     std::deque<Toast> m_toasts;
-
-    ImGuiMenuGame m_menuGame;
 
     // Keep always last
     ImGuiMenuTools m_menuTools;
--- src/dusk/ui/reporting.cpp
+++ src/dusk/ui/reporting.cpp
@@ -26,7 +26,7 @@ CrashReportWindow::CrashReportWindow() : WindowSmall("modal", "modal-dialog") {
     auto* intro = append(mDialog, "div");
     intro->SetClass("modal-body", true);
     intro->SetInnerRML(
-        "Dusk can automatically send crash reports to the developers. Crash reports contain the "
+        "Dusklight can automatically send crash reports to the developers. Crash reports contain the "
         "following:"
         "<br/>• Operating system version<br/>• CPU architecture<br/>• GPU model & driver version"
         "<br/>• File paths (may include account username)<br/>• Stack trace<br/><br/>"
